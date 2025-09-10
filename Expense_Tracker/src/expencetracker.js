34fd1369e64e4a8a835753fb2b603645e43d3a92
Muhammad Tahir <2024cs704@student.uet.edu.pk>
2025-09-11 00:21:55 +0500

    Added CRUD Operations ,  Filterings and Total

--- Expense_Tracker/src/expencetracker.js
+++ Expense_Tracker/src/expencetracker.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
 import {pie} from 'react-chartjs-2';
 import {Chart as ChartJs,ArcElement , Tooltip, Legend} from  'chart.js'
-
+import {useMemo} from 'react'
 ChartJS.register(ArcElement,Tooltip,Legend);
 
 function ExpenseTracker(){
@@ -10,6 +10,7 @@ function ExpenseTracker(){
     const [amt,setAmt]=useState("");
     const [cat,setCAt]=useState("");
     const [type,setType]=useState("");
+    const [filter,setFilter]= useState('All')
   // Load from localStorage on mount
     useEffect(()=>{
         const saved = localStorage.getItem('expenses');
@@ -19,10 +20,58 @@ function ExpenseTracker(){
   // Save to localStorage on change
   useEffect(()=>{
     localStorage.setItem('éxpenss',JSON.stringify(expenses));
-  }, [expenses]);eturn (
+  }, [expenses]);
+const total = useMemo(()=>{ //usememo memorizes the computational result of computation 
+  //this hook will only run when its dependencies change here in case eexpences
+  return expenses.reduce((sum,e)=>//reduce function takes 2 parameter   
+  //sum is the running total here and e is current value
+    e.type==='Income' ? sum + e.amt  : sum - e.amt,0);
+  },[expenses]);
+  const filteredExpenses = useMemo(()=>{
+    if (filter === 'All')return expenses;
+    return expenses.filtter(e=> e.cat===filter ||e.type===filter);
+
+  },[expenses,filter]);
+
+
+
+  const addExpense =()=>{
+    if (!desc || !amt) return;
+    setExpenses([...expenses,{id:Date.now(),desc,amt:parseFloat(amt),cat,type,date:new Date().toISOString()}]);
+    setDesc('');
+    setAmt('');
+  }
+  const deleteExpense=()=>{
+    setExpenses(expenses.filter(e=>e.id!==id));
+  }
+  const editExpense =(id,updated)=>{
+      setExpenses(expenses.map(e => e.id === id ? {...e, ...updated } : e));
+
+
+  }
+
+  return (
     <div>
       <h1>Advanced Expense Tracker</h1>
       {/* Form and list will go here */}
+      <input value={desc} onChange={(e)=>{setDesc(e.target.desc)}}/>
+      <input value={amt}type="number"onChange={(e) => setAmt(e.target.value)} placeholder="Amount"  />
+      <select value={cat} onChange={(e) => setCat(e.target.value)}>
+  <option>Food</option><option>Transport</option><option>Entertainment</option>
+</select>
+<h2> Total : ${total.toFixed(2)}</h2>
+<select value={filter} onChange={(e)=> setFilter(e.target.value)}>
+  <option>All</option><option>Food</option><option>Income</option><option>Expense</option>
+</select>
+<ul>
+  {filteredExpenses.map(e=>(
+            <li key={e.id}>
+         {e.desc} - ${e.amt} ({e.cat}, {e.type}) - {new Date(e.date).toLocaleDateString()}
+      <button onClick={() => deleteExpense(e.id)}>Delete</button>
+    </li>
+  ))}
+</ul>
+
     </div>
   );
 }
